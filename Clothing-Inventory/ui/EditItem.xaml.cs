6bac1bfe21b4f839c878a1fcf3529827e4588ea1
Jocelyn <fries464@myumanitoba.ca>
2026-01-04 19:53:22 -0600

    Began creating a UserControl for editing components of a piece of clothing

---
+++ Clothing-Inventory/ui/EditItem.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Clothing_Inventory.ui
+{
+    /// <summary>
+    /// Interaction logic for EditItem.xaml
+    /// </summary>
+    public partial class EditItem : UserControl
+    {
+        public EditItem()
+        {
+            InitializeComponent();
+        }
+    }
+}
